85408d533d85ce1cdea9ae340b52933798a22553
SamnangKong426 <samnangkong499@gmail.com>
2025-03-13 13:09:45 +0700

    last

--- w5/lib/EXERCISE-2/screen/home.dart
+++ w5/lib/EXERCISE-2/screen/home.dart
@@ -1,3 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/count_notifier.dart';
+
+enum CardType { red, blue }
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    print("Home rebuild");
+    return Consumer<ColorCounters>(
+      builder: (BuildContext context, counter, Widget? child) {
+        // ignore: avoid_print
+        print("Consumer builder called");
+        return Scaffold(
+          body:
+              _currentIndex == 0
+                  ? ColorTapsScreen(
+                    redTapCount: counter.redCount,
+                    blueTapCount: counter.blueCount,
+
+                    // 5. Use the colorCounters to get or increment the colors
+                    onRedTap: () {
+                      counter.incrementRed();
+                    },
+                    onBlueTap: () {
+                      counter.incrementBlue();
+                    },
+                  )
+                  : StatisticsScreen(
+                    redTapCount: counter.redCount,
+                    blueTapCount: counter.blueCount,
+                  ),
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.tap_and_play),
+                label: 'Taps',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bar_chart),
+                label: 'Statistics',
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class ColorTapsScreen extends StatefulWidget {
+  final int redTapCount;
+  final int blueTapCount;
+  final VoidCallback onRedTap;
+  final VoidCallback onBlueTap;
+
+  const ColorTapsScreen({
+    super.key,
+    required this.redTapCount,
+    required this.blueTapCount,
+    required this.onRedTap,
+    required this.onBlueTap,
+  });
+
+  @override
+  ColorTapsScreenState createState() => ColorTapsScreenState();
+}
+
+class ColorTapsScreenState extends State<ColorTapsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Color Taps')),
+      body: Column(
+        children: [
+          ColorTap(
+            type: CardType.red,
+            tapCount: widget.redTapCount,
+            onTap: widget.onRedTap,
+          ),
+          ColorTap(
+            type: CardType.blue,
+            tapCount: widget.blueTapCount,
+            onTap: widget.onBlueTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ColorTap extends StatelessWidget {
+  final CardType type;
+  final int tapCount;
+  final VoidCallback onTap;
+
+  const ColorTap({
+    super.key,
+    required this.type,
+    required this.tapCount,
+    required this.onTap,
+  });
+
+  Color get backgroundColor => type == CardType.red ? Colors.red : Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        width: double.infinity,
+        height: 100,
+        child: Center(
+          child: Text(
+            'Taps: $tapCount',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class StatisticsScreen extends StatelessWidget {
   final int redTapCount;
   final int blueTapCount;
